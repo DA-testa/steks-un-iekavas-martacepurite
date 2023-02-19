328dd7dfaa3b00e3fee6b0c6453b53b847d26640
MC <martacepurite@gmail.com>
2023-02-19 14:10:20 +0200

    y

--- main.py
+++ main.py
@@ -43,60 +43,15 @@ def main():
 
     ##print(text)
 
-    
-    
-
     if(text=="I"):
         t=input()
         ##print(b)
         find_mismatch(t)
 
-    # elif(text=="F"):
-        
-    #     file=open(text,"r")
-    #     find_mismatch(file.read())
-    #     file.close()
-    
-    # elif(text.startswith("I")):
-    #     spl=text.split()
-    #     find_mismatch(spl.pop())
-    
-    elif(text.startswith("F")):
-        spl=text.split()
-        file=open(spl.pop(),"r")
-        find_mismatch(file.read())
-        file.close()
-
-    # else:
-    #     find_mismatch(text)
-
-
-    # else:
-    #     spl=text.split()
-    #     ##print(spl)
-    #     if(len(spl)>=2):
-    #         b=spl.pop()
-    #         a=spl.pop()
-
-    #         ##print(a)
-    #         ##print(b)
-            
-    #         if(a=='I'):
-    #             find_mismatch(b)
-    #         if(a=='F'):
-    #             filename=input()
-    #             file=open(filename,"r")
-    #             find_mismatch(file.read())
-
-        
-    
-    
-
-    
-
-    
-
-   
+    if(text.startswith("F")):
+        spl=text.splitlines()
+        file=open(spl.pop,"r")
+        find_mismatch(file)
 
 if __name__ == "__main__":
     main()
